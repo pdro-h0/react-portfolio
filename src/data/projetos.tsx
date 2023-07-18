de5f523b71dbf7ee03f0928e1a3626f80ef1b0dd
pdro-h0 <pedro.pho792@gmail.com>
2023-07-18 13:43:12 -0300

    Adicionando 2 novos projetos

--- src/data/projetos.tsx
+++ src/data/projetos.tsx
@@ -1,40 +1,56 @@
-import devmemory from '../assets/devmemory.jpg'
-import frontweek from '../assets/frontweek.jpg'
-import pomodoroxp from '../assets/pomodoroxp.jpg'
-import cachorro from '../assets/cachorro.jpg'
-import doctorcare from '../assets/doctorcare.jpg'
+import devmemory from '../assets/projects/devmemory.jpg'
+import frontweek from '../assets/projects/frontweek.jpg'
+import pomodoroxp from '../assets/projects/pomodoroxp.jpg'
+import cachorro from '../assets/projects/cachorro.jpg'
+import doctorcare from '../assets/projects/doctorcare.jpg'
+import obfF from '../assets/projects/feMain.jpg'
+import obfB from '../assets/projects/beMain.jpg'
 
 export const projetos = [
     {
         id: 1,
+        image: obfF,
+        title: "OneBitFlix(Front end)",
+        description:'Onebitflix é um projeto full-stack desenvolvido no curso da OneBitCode. Nele eu realizei o front-end de uma aplicação EAD de cursos inspirado no netflix.',
+        link: "https://github.com/pdro-h0/onebitflix-front-end"
+    },
+    {
+        id: 2,
+        image: obfB,
+        title: "OneBitFlix(Back end)",
+        description:'Onebitflix é um projeto full-stack desenvolvido no curso da OneBitCode. Nele eu realizei o back-end de uma aplicação EAD de cursos inspirado no netflix.',
+        link: "https://github.com/pdro-h0/onebitflix-back-end"
+    },
+    {
+        id: 3,
         image: devmemory,
         title: "DevMemory",
         description:'O "DevMomory" é um Jogo da Memória virtual.',
         link: "https://github.com/pdro-h0/devmemory"
     },
     {
-        id: 2,
+        id: 4,
         image: frontweek,
         title: "Pearson Hardman",
         description:'O "Pearson Hardman" é um site para ajudar um escritório de advocacia fictício.',
         link: "https://pdro-h0.github.io/pearsonhardman-/"
     },
     {
-        id: 3,
+        id: 5,
         image: pomodoroxp,
         title: "MoveIt",
         description:'O "MoveIt" é um projeto que utiliza a técnica de pomodoro, com o objetivo de melhorar sua produtividade e foco.',
         link: "https://github.com/pdro-h0/moveit"
     },
     {
-        id: 4,
+        id: 6,
         image: cachorro,
         title: "Cachorro",
         description:'O "Cachorro" é um site para ajudar um petshop fictício.',
         link: "https://pdro-h0.github.io/cachorro/"
     },
     {
-        id: 5,
+        id: 7,
         image: doctorcare,
         title: "DoctorCare",
         description:'O "DoctorCare" é um site para ajudar uma clínica hospitalar  fictício.',
