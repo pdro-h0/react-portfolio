c461c5cebc60b1a760b3c159f298d5f033639056
pdro-h0 <pedro.pho792@gmail.com>
2024-02-06 20:57:37 -0300

    Adicionando Radix-ui dialog

--- src/components/Qualification/index.tsx
+++ src/components/Qualification/index.tsx
@@ -1,23 +1,46 @@
 import * as C from "./styles";
 import { certificados } from "../../data/certificados";
 
+import * as Dialog from "@radix-ui/react-dialog";
+import { BiX } from "react-icons/bi";
+
 export const Qualification = () => {
   return (
     <C.Main>
       <h2>Formação</h2>
 
       <p>
-        Sou graduado em Ciência da Computação pelo Centro
-        Universitário Sul de Minas (UNIS) 🎓. Além disso me mantenho sempre
-        atualizado com cursos online.
+        Sou graduado em Ciência da Computação pelo Centro Universitário Sul de
+        Minas (UNIS) 🎓. Além disso me mantenho sempre atualizado com cursos
+        online.
       </p>
 
       <C.Container>
         {certificados.map((certificado) => {
           return (
-            <div key={certificado.id}>
-              <C.Image src={certificado.imgUrl} />
-            </div>
+            <Dialog.Root key={certificado.id}>
+              <Dialog.Trigger asChild>
+                <div>
+                  <C.Image src={certificado.imgUrl} />
+                </div>
+              </Dialog.Trigger>
+
+              <Dialog.Portal>
+                <Dialog.Overlay asChild>
+                  <C.DialogOverlay />
+                </Dialog.Overlay>
+                <Dialog.Content asChild>
+                  <C.DialogContent>
+                    <C.ImageFull src={certificado.imgUrl} />
+                    <Dialog.Close asChild>
+                      <C.DialogClose aria-label="Close">
+                        <BiX />
+                      </C.DialogClose>
+                    </Dialog.Close>
+                  </C.DialogContent>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
           );
         })}
       </C.Container>
--- src/components/Qualification/styles.ts
+++ src/components/Qualification/styles.ts
@@ -17,7 +17,7 @@ export const Container = styled.div`
   margin-top: 3.5rem;
   transition: all ease 0.1s;
 
-   &:hover :not(:hover) {
+  &:hover :not(:hover) {
     opacity: 0.3;
   }
 
@@ -30,7 +30,7 @@ export const Container = styled.div`
   > div {
     transition: all ease 0.2s;
 
-    &:hover{
+    &:hover {
       scale: 1.1;
     }
   }
@@ -41,7 +41,80 @@ export const Image = styled.img`
 
   margin-block: 2.4rem;
 
+  &:hover {
+    cursor: pointer;
+  }
+
   @media (max-width: 820px) {
     max-width: 100%;
+  }
+`;
+
+export const DialogOverlay = styled.div`
+  background-color: black;
+  opacity: 60%;
+  position: fixed;
+  inset: 0;
+  animation: overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+`;
+
+export const DialogContent = styled.div`
+  background-color: white;
+  border-radius: 6px;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
+    hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 75vw;
+  height: 95vh;
+  padding: 20px;
+  animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
+
+  @media (max-width: 768px) {
+    padding: 4px;
+    height: 75vh;
+    width: 90vw;
+  }
+
+  @media (max-width: 430px) {
+    padding: 4px;
+    height: 75vh;
+    width: 90vw;
+  }
+
+  @media (max-width: 430px) {
+    padding: 4px;
+    height: 55vh;
+    width: 90vw;
+  }
+`;
+
+export const ImageFull = styled.img`
+  height: 100%;
+  width: 100%;
+`;
+
+export const DialogClose = styled.button`
+  font-family: inherit;
+  outline: none;
+  border-radius: 100%;
+  border: 1px solid transparent;
+  height: 40px;
+  width: 40px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--tertiary-color);
+  background-color: white;
+  font-size: 90px;
+  position: absolute;
+  top: -14px;
+  right: -12px;
+  object-fit: cover;
+
+  &:hover {
+    cursor: pointer;
   }
 `;
